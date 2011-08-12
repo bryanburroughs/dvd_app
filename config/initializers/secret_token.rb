63161183ea63bbe73d108a145b13bfb93db20993
Bryan Burroughs <bryanburroughs@charter.net>
2011-08-12 08:28:22 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DvdApp::Application.config.secret_token = 'd6702176e89d092fce1af814881ffde938f833d7545aac36b76bd3956f6b6bff01930c4bb79421637958ccce1dc0d4fb75ed175abc678c827774620fa20f0194'
